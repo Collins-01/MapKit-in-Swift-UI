e4442ec54d295ed466b6ca6108948d392832dc35
Collins Oriakhi <collins.oriakhi@itara.co>
2022-04-03 08:13:05 +0100

    Initial Commit

---
+++ MapKit in Swift UI/MapKit_in_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_in_Swift_UIApp.swift
+//  MapKit in Swift UI
+//
+//  Created by Oriakhi Collins on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_in_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
